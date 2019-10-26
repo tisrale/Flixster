2ff3aaef54183a1a675fa26f29f9e0533e51a836
tisrale <tadelejirata@gmail.com>
2019-10-25 20:34:25 -0400

    Activites and Intent system implemented

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,21 +3,35 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.versionedparcelable.ParcelField;
+
+
+@Parcel
 public class Movie {
+    int movieId;
     String posterpath;
     String title;
     String overview;
     String backdropPath;
+    double rating;
+
+    // empty constructor needed by the Parceler library
+    public  Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterpath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating =  jsonObject.getDouble("vote_average");
+        movieId =  jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJSONArray(JSONArray movieJSONArray)throws JSONException{
@@ -57,5 +71,14 @@ public class Movie {
     }
     public String getBackdroppath(){
         return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
+
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
